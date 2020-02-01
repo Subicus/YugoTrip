bd73dd95d0f9e6189b0e766601ddc6a4cfc10e41
Sava Minic <savam@nordeus.com>
2020-02-01 10:03:00 +0100

    speedometer

---
+++ Assets/Scripts/Speedometer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Speedometer : MonoBehaviour
+{
+    #region Properties
+
+    public Transform Needle;
+
+    [Range(0,1)]
+    public float RotationNormalized;
+
+    public float MinRotation;
+    public float MaxRotation;
+
+    #endregion
+
+    #region Update
+
+    public void Update()
+    {
+        Needle.rotation = Quaternion.Euler(0f, 0f, Mathf.Lerp(MinRotation,MaxRotation,RotationNormalized));
+    }
+
+    #endregion
+}

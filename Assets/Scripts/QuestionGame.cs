8b86dc540bb6390e308cdebb821b66eec5220989
Sava Minic <savam@nordeus.com>
2020-02-02 00:25:08 +0100

    unity editor remove

--- Assets/Scripts/QuestionGame.cs
+++ Assets/Scripts/QuestionGame.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Animations;
 using UnityEngine;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
